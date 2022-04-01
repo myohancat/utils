24a047eea11c667987d14317f807f3a9dffd3962
myohancat@naver.com <myohancat@naver.com>
2022-04-01 15:32:36 +0900

    kykim: add trim code

---
+++ trim.c
@@ -0,0 +1,44 @@
+#include "trim.h"
+
+#include <stdio.h>
+#include <string.h>
+
+#define ISSPACE(c) 	((c)==' ' || (c)=='\f' || (c)=='\n' || (c)=='\r' || (c)=='\t' || (c)=='\v')
+
+char* ltrim(char *s) 
+{
+	if(!s) return s;
+
+	while(ISSPACE(*s)) s++;
+
+	return s;
+}
+
+char* rtrim(char* s) 
+{
+	char* back;
+
+	if(!s) return s;
+
+	back = s + strlen(s);
+
+	while((s <= --back) && ISSPACE(*back));
+	*(back + 1)	= '\0';
+
+	return s;
+}
+
+#define TEST_MAIN
+#ifdef TEST_MAIN
+int main(int argc, char* arv[])
+{
+	char testStr[] = "     HaHa   HeHe   ";
+
+	printf("Orginal : \"%s\"\n", testStr);
+	printf("ltrim   : \"%s\"\n", ltrim(testStr));
+	printf("rtrim   : \"%s\"\n", rtrim(testStr));
+	printf("trim    : \"%s\"\n", trim(testStr));
+
+	return 0;
+}
+#endif
---
+++ trim.h
@@ -0,0 +1,9 @@
+#ifndef __TRIM_H_
+#define __TRIM_H_
+
+char* ltrim(char *s);
+char* rtrim(char* s);
+
+#define trim(s)		rtrim(ltrim(s))
+
+#endif /*__TRIM_H_*/
